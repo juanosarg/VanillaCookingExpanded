999b253341574bd7a5e8de417efdb08f60087762
Mhburg <Morgan_lee5@hotmail.com>
2020-07-23 16:17:07 -0400

    Reset tolerance for dessert to 0 after cosuming one
    Clean up trace

---
+++ 1.1/Source/VanillaCookingExpanded/VanillaCookingExpanded/Utils/DessertDefs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace VanillaCookingExpanded.Utils
+{
+    [DefOf]
+    public static class DessertDefs
+    {
+        private static List<ThingDef> _desrtDefs;
+
+        public static ThingDef VCE_SimpleDessert;
+
+        public static ThingDef VCE_FineDessert;
+
+        public static ThingDef VCE_LavishDessert;
+
+        public static ThingDef VCE_GourmetDessert;
+
+        public static List<ThingDef> AllDeserts =>
+            _desrtDefs ?? (_desrtDefs = new List<ThingDef>()
+                {VCE_SimpleDessert, VCE_FineDessert, VCE_LavishDessert, VCE_GourmetDessert});
+    }
+}
